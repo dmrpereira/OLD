53760e47d38bc4dd581fa8237be1abc903e44507
dmrpereira <dmrpereira@gmail.com>
2014-01-06 12:36:32 +0000

    Now, definitely restarting implementation structure

--- util.ads
+++ util.ads
@@ -1,9 +1,21 @@
+with Ada.Strings; use Ada.Strings;
+
 package Util is
    
+   -- Binary numbers
    subtype Bin_Num is Natural range 0..1;
-   subtype Hex_Num is Natural range 0..15;
-   subtype Array_Limit is Natural range 0 .. 64;
-   type Bin_Array is array(Array_Limit) of Bin_Num;
-   type Hex_Array is array(Array_Limit) of Hex_Num;
+      
+   -- Access to string datatypes   
+   type String_Access is access all String;
+   
+   -- The type of binary arrays for identifier indexing, 
+   -- and access types to them
+   type Idxs_Array is array(Natural range <>) of Bin_Num;
+   type Idxs_Array_Access is access all Idxs_Array;
+   
+   --subtype Hex_Num is Natural range 0..15;
+   --subtype Array_Limit is Natural;
+   --type Bin_Array is array(Array_Limit) of Bin_Num;
+   --type Hex_Array is array(Array_Limit) of Hex_Num;
       
 end Util;
