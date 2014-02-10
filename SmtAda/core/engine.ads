ddcc052a41ca9982c870947607714dd1f32b812d
dmrpereira <dmrpereira@gmail.com>
2014-02-10 13:00:39 +0000

    cool

--- SmtAda/core/engine.ads
+++
@@ -1,8 +0,0 @@
-package Engine is
-   
-   type Instr_Aux is abstract tagged null record;
-   type Instr is access all Instr_Aux'Class;
-   
-   function Image(I:in Instr_Aux) return String is abstract;
-   
-end Engine;
--- SmtAda/terms/ast_functions.adb
+++
@@ -1,8 +0,0 @@
-package body Ast_Functions is
-   
-   function App(I:in Ident;L:in Expr_List) return Expr is
-   begin
-      return Build_Eapp(I,Undef_Stype,L);
-   end App;
-   
-end Ast_Functions;
